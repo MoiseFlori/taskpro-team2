7d3fe3db1c6f5306e4d521253642cf477746077d
MoiseFlori <moiseflori@outlook.com>
2025-06-20 15:50:54 +0300

    add base structure

---
+++ frontend/src/utils/axiosConfig.js
@@ -0,0 +1,16 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+// });
+
+// export const setAuthToken = (token) => {
+//   if (token) {
+//     const authHeader = `Bearer ${token}`;
+//     instance.defaults.headers.common["Authorization"] = authHeader;
+//   } else {
+//     delete instance.defaults.headers.common["Authorization"];
+//   }
+// };
+
+// export default instance;
