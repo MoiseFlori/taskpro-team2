754792d7e74b6c816eba0a6dc6ab0ab88eac25a0
MoiseFlori <moiseflori@outlook.com>
2025-06-23 18:26:56 +0300

    update db.js and server.js

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 const DB_URI = process.env.DB_URI;
@@ -11,18 +13,3 @@ mongoose
     console.error("Database connection error:", error.message);
     process.exit(1);
   });
-
-const connectDB = async () => {
-  const uri = process.env.MONGODB_URI;
-  if (!uri) throw new Error("❌ MONGODB_URI lipsește din .env");
-
-  try {
-    await mongoose.connect(uri);
-    console.log("✅ Conectat la:", mongoose.connection.name);
-  } catch (err) {
-    console.error("❌ Eroare la conectarea MongoDB:", err.message);
-    throw err;
-  }
-};
-
-module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,11 @@
 require("dotenv").config();
-console.log("📂 MONGODB_URI =", process.env.MONGODB_URI);
+
+
 
 const app = require("./app");
-const connectDB = require("./db");
-
 const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server is running. Use our API on port: ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running. Use our API on port: ${PORT}`);
+});
 
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`🚀 Server pornit la http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("💥 Serverul nu a pornit din cauza MongoDB:", err.message);
-  });
